53bd4e07785df428fb93d0861e7e3f506fd89db9
sayex <elsayer@msn.com>
2019-04-15 00:07:18 -0600

    lint cleanup

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,49 +2,43 @@ const db = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
-    db.Users
-      .findAll({attributes: { exclude: ['password']}})
+    db.Users.findAll({ attributes: { exclude: ["password"] } })
       .then(dbUsers => res.json(dbUsers))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Users
-      .findOne({attributes: { exclude: ['password']}, where: {email: req.params.email}})
+    db.Users.findOne({ attributes: { exclude: ["password"] }, where: { email: req.params.email } })
       .then(dbUsers => res.json(dbUsers))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Users
-      .create(req.body)
+    db.Users.create(req.body)
       .then(dbUsers => res.json(dbUsers))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Users
-      .update(req.body, {where: {email: req.params.email }})
+    db.Users.update(req.body, { where: { email: req.params.email } })
       .then(dbUsers => res.json(dbUsers))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Users
-      .destroy({where: {email: req.params.email}})
+    db.Users.destroy({ where: { email: req.params.email } })
       .then(dbUsers => res.json(dbUsers))
       .catch(err => res.status(422).json(err));
   },
-  auth: function(req, res){
-      db.Users
-      .findOne({where: {
-          email: req.params.email
-      }})
-      .then(function(dbUsers) {
-        if (!dbUsers) {
-          res.send("No User Found")
-          }
-        else if (!dbUsers.validPassword(req.body.password)) {
-          res.send("Incorrect Password")
-        } else {
-        res.send("Authenticated")
-        }
-      });
+  auth: function(req, res) {
+    db.Users.findOne({
+      where: {
+        email: req.params.email
+      }
+    }).then(function(dbUsers) {
+      if (!dbUsers) {
+        res.send("No User Found");
+      } else if (!dbUsers.validPassword(req.body.password)) {
+        res.send("Incorrect Password");
+      } else if (dbUsers.validPassword(req.body.password)) {
+        res.send("Authenticated");
+      }
+    });
   }
 };
