3ea60d724a18d5c107928dd32094a52b802058b2
JD <jdfili14@gmail.com>
2019-04-15 19:32:24 -0700

    added react

---
+++ client/public/assets/js/google-analytics.js
@@ -0,0 +1,1 @@
+// Place your Google Analytics Code Here
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Nav from "./components/Nav"
+
+class App extends Component {
+  render() {
+    return (
+      <Nav />
+    );
+  }
+}
+
+export default App;
