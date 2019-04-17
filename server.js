b1f41ce8e4ede4768782b856d91aa6478b3c0797
sayex <elsayer@msn.com>
2019-04-17 16:20:30 -0600

    force sync false

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("html"));
 app.options("*", cors());
 app.use(routes);
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
   });
