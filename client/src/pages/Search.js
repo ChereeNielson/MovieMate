8832b6419446158d5752b2cc65a755ac5e88eb89
JD <jdfili14@gmail.com>
2019-04-23 19:43:51 -0600

    can toggle between watchlist and recommended

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -35,6 +35,7 @@ class Search extends Component {
 
         })
     }
+  
     search = query => {
         this.reset();
         API.search(query)
--- client/src/pages/WatchList.js
+++ client/src/pages/WatchList.js
@@ -10,11 +10,13 @@ import RemoveRecBtn from "../components/RemoveRecBtn";
 import API from "../utils/API";
 class WatchList extends Component {
     state = {
-        results: []
+        results: [],
+        selectValue: ''
     }
 
     componentDidMount() {
         this.getResults();
+        this.getInitialState();
     }
 
     getResults = () => {
@@ -23,8 +25,14 @@ class WatchList extends Component {
             this.setState({ results: res.data })
         })
     }
+    getInitialState = () => {
+        this.setState({ selectValue: "current watchlist" })
+    }
+    handleChange = e => {
+        this.setState({ selectValue: e.target.value });
+    }
     removeWatchItem = (id) => {
-        
+
         const filtered = this.state.results.filter(res => res.id != id);
         this.setState({ results: filtered });
         API.deleteWatchListItem(id).then(res => {
@@ -33,25 +41,25 @@ class WatchList extends Component {
     }
 
     recommend = (id) => {
-     
- 
+
+
         let data = {
             recommend: 1
         }
-        API.recommendUpdate(id, data).then(res =>{
+        API.recommendUpdate(id, data).then(res => {
             this.getResults()
         });
 
     }
 
     removeRec = (id) => {
-      
-        
+
+
         let data = {
             recommend: 0
         }
 
-        API.recommendUpdate(id, data).then(res=>{
+        API.recommendUpdate(id, data).then(res => {
             this.getResults()
         });
     }
@@ -64,23 +72,55 @@ class WatchList extends Component {
                     title="Watch List"
                 />
                 <Wrapper>
-                    
-                    {this.state.results.map(res => {
-                        return (
-                            <div>
-                                
-                                <WatchListItem
-                                    title={res.title}
-                                    plot={res.synopsis ? res.synopsis : "no data"}
-                                    image={res.image}
-                                    rec = {res.recommend}
-                                    recommend={() => this.recommend(res.id)}
-                                    removeRec={()=> this.removeRec(res.id)}
-                                    removeFromList={() => this.removeWatchItem(res.id)}
-                                />
+                    <div className="row mb50">
+                        <div className="col-md-6" />
+                        <div className="col-md-6">
+                            {/* Sort by */}
+                            <div className="sort-by mr-4 mb-3">
+                                <div className="sort-by-select">
+                                    <select onChange={this.handleChange} value={this.state.selectValue} className="chosen-select-no-single">
+                                        <option value="current watchlist">Current Watchlist</option>
+                                        <option value="recommendations">Recommendations</option>
+                                    </select>
+                                </div>
                             </div>
-                        )
-                    })}
+                        </div>
+                    </div>
+                    {this.state.selectValue === "current watchlist" ?
+                        this.state.results.map(res => {
+                            return (
+                                <div>
+
+                                    <WatchListItem
+                                        title={res.title}
+                                        plot={res.synopsis ? res.synopsis : "no data"}
+                                        image={res.image}
+                                        rec={res.recommend}
+                                        recommend={() => this.recommend(res.id)}
+                                        removeRec={() => this.removeRec(res.id)}
+                                        removeFromList={() => this.removeWatchItem(res.id)}
+                                    />
+                                </div>
+                            )
+                        }) : this.state.results.map(res => {
+                            if (res.recommend) {
+                                return (
+                                    <div>
+                                        <WatchListItem
+                                            title={res.title}
+                                            plot={res.synopsis ? res.synopsis : "no data"}
+                                            image={res.image}
+                                            rec={res.recommend}
+                                            recommend={() => this.recommend(res.id)}
+                                            removeRec={() => this.removeRec(res.id)}
+                                            removeFromList={() => this.removeWatchItem(res.id)}
+                                        />
+                                    </div>
+
+                                )
+                            }
+
+                    }) }
 
                 </Wrapper>
 
